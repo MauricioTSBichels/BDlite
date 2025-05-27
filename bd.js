3e961538b702c9cf4eb96d1f98fc2e757c706a9a
MAURÍCIO TELLES SILVA BICHELS <mauricio_bichels@estudante.sesisenai.org.br>
2025-05-27 10:08:29 -0300

    DB

--- bd.js
+++ bd.js
@@ -3,3 +3,26 @@ const sqlite=require('sqlite3')
 const db= new sqlite.Database('./teste.db',(err)=>{
    console.log(err) 
 })
+
+let sql=`CREATE TABLE IF NOT EXISTS usuarios(
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
+ nome TEXT,
+ cpf TEXT,
+ email TEXT UNIQUE
+)`
+
+db.exec(sql, (err)=>{
+   console.log(err) 
+})
+
+let sql2=`INSERT INTO usuarios (nome, cpf, email) VALUES(?, ?, ?)`
+
+db.run(sql2,['Mauricio','28747543287275','Mauricio@gmail.com'],(data,err)=>{
+    if (err){
+        console.log(err)
+        return
+    }
+    else{
+        console.log(data)
+    }
+})
